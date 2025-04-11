d7858cde3b5c09033c955663395edccf8a7ab05c
Youssef Amr Mohamed <yousef240103223@sut.edu.eg>
2025-04-11 15:30:42 +0200

    Add files via upload

---
+++ HospitalManagementSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hospitalmanagementsystem;
+
+/**
+ *
+ * @author hp
+ */
+public class HospitalManagementSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
